cb4b0238b22592a89373060076d6a14cd0ff3eb5
sjurgust <sjurandreaslundstein@gmail.com>
2018-05-04 15:52:02 +0200

    update med nye klipp

--- app.js
+++ app.js
@@ -7,19 +7,29 @@ app = require('express.io')()
 app.http().io()
 
 const videolist = [
-  {url:'grey.mp4', content_type:'video/mp4', poster:'grey.png'},//grey
-  {url:'cris_og_faren.m4v', content_type:'video/mp4', poster:'cris_og_faren.png'}, //1
-  {url:'Katten_henter_en_nerf-sett54ganger.mp4', content_type:'video/mp4', poster:'Katten_henter_en_nerf-sett54ganger.png'},//2
-  {url:'Balloons 3D Live Wallpaper-kqvdE_yZ84I.mp4', content_type:'video/mp4', poster:'proxy.png'},//3
-  {url:'grey.mp4', content_type:'video/mp4', poster:'ek204.png'},//4
-  {url:'Making DIY Slim Gone Wrong-NybSLhWzqC4.mp4', content_type:'video/mp4', poster:'Thumb-Making-DIY-Slim-Gone-Wrong.jpg'},//5 Naif jr
-  {url:'Giving a shoutout To-9nP_HnvDjYA.mp4', content_type:'video/mp4', poster:'Thumb-Giving-a-shoutout-To-9nP_HnvDjYA.jpg'},//6 Naif jr
-  {url:'GOT A Legendary Chest OMG!!!-MEGzuz80nEg.mp4', content_type:'video/mp4', poster:'thumb-GOT-A-Legendary-Chest-OMG!!!-MEGzuz80nEg.f140.jpg'},//7 Naif jr
-  {url:'Visiting sharks-z36yo4AvFhA.mp4', content_type:'video/mp4', poster:'thumb-Visiting-sharks-z36yo4AvFhA.jpg'},//8 Naif jr
+  {id:0, url:'grey.mp4', content_type:'video/mp4', poster:'grey.png'},//grey
+  {id:1, url:'cris_og_faren.m4v', content_type:'video/mp4', poster:'cris_og_faren.png'}, //1
+  {id:2, url:'Katten_henter_en_nerf-sett54ganger.mp4', content_type:'video/mp4', poster:'Katten_henter_en_nerf-sett54ganger.png'},//2
+  {id:3, url:'Tjubvideo_Delia Flytter inn_med_stopp.mp4', content_type:'video/mp4', poster:'vlcsnap-2018-02-28-16h28m37s015.jpg'},//3
+  {id:4, url:'grey.mp4', content_type:'video/mp4', poster:'ek204.png'},//4
+  {id:5, url:'Making DIY Slim Gone Wrong-NybSLhWzqC4.mp4', content_type:'video/mp4', poster:'Thumb-Making-DIY-Slim-Gone-Wrong.jpg'},//5 Naif jr
+  {id:6, url:'Giving a shoutout To-9nP_HnvDjYA.mp4', content_type:'video/mp4', poster:'Thumb-Giving-a-shoutout-To-9nP_HnvDjYA.jpg'},//6 Naif jr
+  {id:7, url:'GOT A Legendary Chest OMG!!!-MEGzuz80nEg.mp4', content_type:'video/mp4', poster:'thumb-GOT-A-Legendary-Chest-OMG!!!-MEGzuz80nEg.f140.jpg'},//7 Naif jr
+  {id:8, url:'Visiting sharks-z36yo4AvFhA.mp4', content_type:'video/mp4', poster:'thumb-Visiting-sharks-z36yo4AvFhA.jpg'},//8 Naif jr
+  {id:9, url:'', content_type:'video/mp4', poster:''},
+  {id:9, url:'', content_type:'video/mp4', poster:''},
+  {id:11, url:'C005004V0143E34885.mp4', content_type:'video/mp4', poster:'C005004V0143E34885.png'},
+  {id:12, url:'Tjubvideo_Brennende thuja_V2.mp4', content_type:'video/mp4', poster:'vlcsnap-2018-03-12-11h05m09s755.png'},
+  {id:13, url:'Helium_FUNNY_STUFF.mov_5k.mp4', content_type:'video/mp4', poster:'Helium_FUNNY_STUFF.mov_5k.png'},
+  {id:14, url:'Helium_FUNNY_STUFF.private.mp4', content_type:'video/mp4', poster:'Helium_FUNNY_STUFF.private.png'},
+  {id:15, url:'C1002_DORULLMAMMA.mov_10M.mp4', content_type:'video/mp4', poster:'Helium_E_1002_12.sub.01.png'},
+  {id:16, url:'Helium E_1006_18.mov_5k_trimmed.mp4', content_type:'video/mp4', poster:'Helium E_1006_18.mov_5k_trimmed.png'},
+  {id:17, url:'C021002.mov_grill2000.mp4', content_type:'video/mp4', poster:'C021002.mov_grill2000.png'},
+  {id:18, url:'C006006.mov_C1112_tjub.private.mp4', content_type:'video/mp4', poster:'C006006.mov_C1112_tjub.private.png'},
+  {id:19, url:'C006006.mov_C1112_tjub.mp4', content_type:'video/mp4', poster:'C006006.mov_C1112_tjub.png'},
+  '',]
 
-  '',
 
-]
 
 const ThumbnailGenerator = require('video-thumbnail-generator').default;
 
